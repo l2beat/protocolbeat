1992bf085663fbb9273e8203db5cf401c82e28d1
Krzysztof Kaczor <krzkaczor@gmail.com>
2023-02-21 22:28:18 +0100

    Add button to delete nodes

---
+++ packages/frontend/src/api/delete.ts
@@ -0,0 +1,32 @@
+import { SimpleNode } from './SimpleNode'
+
+// prevents removal of the node that is still referenced
+export function deleteNode(
+  nodes: SimpleNode[],
+  idsToRemove: string[],
+): SimpleNode[] {
+  const nodesReferencedFromDeletingNodes = nodes
+    .filter((nodes) => idsToRemove.includes(nodes.id))
+    .flatMap((node) => node.fields)
+    .filter((field) => field.connection)
+    .map((field) => field.connection!)
+
+  if (
+    !nodesReferencedFromDeletingNodes.every((id) => {
+      const itWillBeDeleted = idsToRemove.includes(id)
+      const nodeDoesntExist = !nodes.some((node) => node.id === id)
+
+      return itWillBeDeleted || nodeDoesntExist
+    })
+  ) {
+    throw new Error(
+      'Cannot delete nodes that are still referenced by other nodes',
+    )
+  }
+
+  const nodesAfterRemoval = nodes.filter(
+    (node) => !idsToRemove.includes(node.id),
+  )
+
+  return nodesAfterRemoval
+}
