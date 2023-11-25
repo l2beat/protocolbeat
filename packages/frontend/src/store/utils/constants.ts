d5bd29c00d87ec44523ca63a6f1f28870a9a0f87
Adrian Adamiak <adrian@adamiak.net>
2023-11-25 23:25:03 +0000

    Revert change to zoom sensitivity (will do another PR)

--- packages/frontend/src/store/utils/constants.ts
+++ packages/frontend/src/store/utils/constants.ts
@@ -13,7 +13,7 @@ export const FIELD_HEIGHT = 24
 export const NODE_WIDTH = 200
 export const NODE_SPACING = 25
 
-export const ZOOM_SENSITIVITY = 0.001
+export const ZOOM_SENSITIVITY = 0.02
 export const MAX_ZOOM = 3
 export const MIN_ZOOM = 0.3
 
