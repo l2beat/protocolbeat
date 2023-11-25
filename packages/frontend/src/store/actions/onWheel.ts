d1279657060f92d46594a65afd6895a6a69381b8
Adrian Adamiak <adrian@adamiak.net>
2023-11-25 23:45:19 +0000

    Enhance mouse control
    - middle button always pans (ctrl not necessary)
    - scroll always zooms (no shift necessary)
    - less zoom sensitivity (for the mouse scroll)

--- packages/frontend/src/store/actions/onWheel.ts
+++ packages/frontend/src/store/actions/onWheel.ts
@@ -1,6 +1,5 @@
 import { State } from '../State'
 import {
-  IS_MACOS,
   MAX_ZOOM,
   MIN_ZOOM,
   SCROLL_LINE_HEIGHT,
@@ -17,30 +16,19 @@ export function onWheel(
   const { deltaX, deltaY } = getWheelDelta(event)
   const { offsetX, offsetY, scale } = state.transform
 
-  if (event.ctrlKey || event.metaKey) {
-    const rect = view.getBoundingClientRect()
+  const rect = view.getBoundingClientRect()
 
-    const desiredChange = -deltaY * ZOOM_SENSITIVITY
-    let newScale = scale * (1 + desiredChange)
-    newScale = Math.max(MIN_ZOOM, Math.min(MAX_ZOOM, newScale))
-    const change = newScale / scale - 1
+  const desiredChange = -deltaY * ZOOM_SENSITIVITY
+  let newScale = scale * (1 + desiredChange)
+  newScale = Math.max(MIN_ZOOM, Math.min(MAX_ZOOM, newScale))
+  const change = newScale / scale - 1
 
-    return {
-      transform: {
-        offsetX: offsetX + (rect.left - event.clientX) * change,
-        offsetY: offsetY + (rect.top - event.clientY) * change,
-        scale: scale * (1 + change),
-      },
-    }
-  } else {
-    const invert = event.shiftKey && !IS_MACOS
-    return {
-      transform: {
-        offsetX: offsetX - (!invert ? event.deltaX : deltaY),
-        offsetY: offsetY - (!invert ? event.deltaY : deltaX),
-        scale,
-      },
-    }
+  return {
+    transform: {
+      offsetX: offsetX + (rect.left - event.clientX) * change,
+      offsetY: offsetY + (rect.top - event.clientY) * change,
+      scale: scale * (1 + change),
+    },
   }
 }
 
